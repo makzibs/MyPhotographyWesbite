29886f2763492ec57ce56f1f4693b68eb2b8be14
Makzibs Anil <makzibsanil@MakzibsPro.local>
2026-04-25 15:38:54 -0400

    Update category3 with new hero image and added images, update category4 images

--- src/components/CategoryDetail.js
+++ src/components/CategoryDetail.js
@@ -36,13 +36,13 @@ const CategoryDetail = () => {
     },
     category3: {
       folder: 'category3-people & lifestyle',
-      heroImage: 'heroImage3.jpg',
-      images: ['2.3.jpg', '2.4.jpg', '6.8.jpg', '8.1.jpg', '8.2.jpg', '10.1.jpg', '14.1.jpg', '15.1.jpg', '16.4.jpg', 'DSC08985.jpg']
+      heroImage: 'heroImage14.jpg',
+      images: ['2.3.jpg', '2.4.jpg', '3.8.jpg', '6.8.jpg', '8.1.jpg', '8.2.jpg', '14.1.jpg', '15.1.jpg', '16.4.jpg', 'DSC06044.jpg', 'DSC06047.jpg', 'DSC08985.jpg']
     },
     category4: {
       folder: 'category4-wedding',
       heroImage: 'heroImage4.jpg',
-      images: ['4.1.jpg', '4.4.jpg', '4.5.jpg', '4.6.jpg', '4.7.jpg', '4.8.jpg']
+      images: ['4.1.jpg', '4.4.jpg', '4.5.jpg', '4.7.jpg', '4.8.jpg', 'DSC09118.jpg']
     },
     category5: {
       folder: 'category5-event-photography',
