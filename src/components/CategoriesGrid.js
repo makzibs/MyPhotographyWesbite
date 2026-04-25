44f68605798897a8258735d021f68023472e2617
Makzibs Anil <makzibsanil@MakzibsPro.local>
2026-04-25 17:12:31 -0400

    Add ScrollingText and CTA section to categories page

--- src/components/CategoriesGrid.js
+++ src/components/CategoriesGrid.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CategoryGrid from './CategoryGrid';
+import ScrollingText from './ScrollingText';
 import './CategoryGrid.css';
 
 const CategoriesGrid = () => {
   return (
     <div className="categories-grid-container">
       <CategoryGrid />
+      
+      {/* Photography quotes and keywords section */}
+      <ScrollingText />
+      
+      <div className="cta-section">
+        <h2>LET'S WORK</h2>
+        <Link to="/contact" className="cta-button">GET IN TOUCH</Link>
+      </div>
     </div>
   );
 };
